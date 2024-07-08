24ae14a60024e41e26184b8f4b2c93636396bbfb
AbdMamun1999 <mamun12121999@gmail.com>
2024-07-08 23:02:37 +0600

    solved one problem

---
+++ week_fifteen/A_Short_Sort.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+    if(s=="bca"||s=="cab") cout << "NO" << '\n';
+    else cout << "YES" << '\n';
+    }
+    
+    return 0;
+}
