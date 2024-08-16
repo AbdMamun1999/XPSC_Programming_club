f291cb4b55a5645203966350c26aaed53ccfeb6a
AbdMamun1999 <mamun12121999@gmail.com>
2024-08-16 22:58:08 +0600

    upload a file

--- week_seventeen/day_six/C_Choose_the_Different_Ones.cpp
+++ week_seventeen/day_six/C_Choose_the_Different_Ones.cpp
@@ -5,6 +5,7 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
  
+    
     int t;
     cin >> t;
     while (t--)
@@ -13,72 +14,47 @@ int main() {
         cin >> n >> m >> k;
 
         int a[n],b[m];
+        vector<int>visited(k+1,0);
         for (int i = 0; i < n; i++)
         {
             cin >> a[i];
+            if(a[i] <= k)
+                visited[a[i]] = 1;
         }
         
         for (int i = 0; i < m; i++)
         {
             cin >> b[i];
-        }
-
-        vector<int>n1(k+1,0);
-        for (int i = 0; i < n; i++)
-        {
-            if(a[i] <= k){
-                n1[a[i]] = 1;
-            }
-        }
-
-        vector<int>n2(k+1,0);
-        for (int i = 0; i < m; i++)
-        {
             if(b[i] <= k){
-                n2[b[i]] = 1;
+                if(visited[b[i]]) visited[b[i]] = 3;
+                else visited[b[i]] = 2;
             }
         }
 
-        int cnt1 = 0,cnt2 = 0;
-
+        int cnt1 = k/2,cnt2 = k/2;
+        bool flag = true;
         for (int i = 1; i <= k; i++)
         {
-            if(n1[i]==1)cnt1++;
+            // cout << visited[i] << " ";
+            if(visited[i]==1) cnt1--;
+            else if(visited[i]==2) cnt2--;
+            else if(visited[i]==0){
+                flag = false;
+                break;
+            }
         }
+        cout << visited.size() << endl;
+        // cout << endl;
+        // cout << cnt1 << " " << cnt2 << endl;
+        // cout << flag << endl;
+        // if(flag==false) cout << "NO" << '\n';
+        // else {
+        //     if(cnt1 >= 0 && cnt2 >= 0) cout << "YES" << '\n';
+        //     else cout << "NO" << '\n';
+        // }
 
-        for (int i = 1; i <= k; i++)
-        {
-            if(n2[i]==1)cnt2++;
-        }
         
-
-        if((cnt1 > k/2 && cnt2 > k/2) || (cnt1 == k/2 && cnt2 > k/2) || (cnt1 > k/2 && cnt2 == k/2)) cout << "YES" << '\n';
-        else if(cnt1==k/2 && cnt2==k/2)
-        {
-            vector<int>res(k+1,0);
-            for (int i = 1; i <= k; i++)
-            {
-                if(n1[i]==1) res[i]=1;
-            }
-
-            for (int i = 1; i <= k; i++)
-            {
-                if(n2[i]==1) res[i]=1;
-            }
-
-            int cnt = 0;
-            for (int i = 1; i <= k; i++)
-            {
-                if(res[i]==1)cnt++;
-            }
-
-
-            if(cnt == k) cout << "YES" << '\n';
-            else cout << "NO" << '\n';
-            
-            
-        }
-        else cout << "NO" << '\n';
     }
+    
     return 0;
 }
